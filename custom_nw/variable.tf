c993e43584d8375bed550867231f7ecaa8a08fea
sam <sammathloka9@gmail.com>
2024-03-13 17:02:48 -0400

    var changes

--- custom_nw/variable.tf
+++ custom_nw/variable.tf
@@ -1,23 +1,23 @@
 variable "ami_id" {
     description = "passing ami value to main.tf"
     type = string
-    default = "ami-0d7a109bf30624c99"
+    default = ""
 }
 
 variable "instance_type" {
     type = string
-    default = "t2.micro"
+    default = ""
   
 }
 
 variable "key_name" {
     type = string
-    default = "Sam-keypair"
+    default = ""
   
 }
 
 variable "tags" {
     type = string
-    default = "sam_ec2"
+    default = ""
   
 }
